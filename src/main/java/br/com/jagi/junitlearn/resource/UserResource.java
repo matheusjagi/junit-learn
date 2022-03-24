50f299bc64039299584f0c1f4b620f9596708405
Matheus Jagi <matheus.jagi@basis.com.br>
2022-03-23 23:31:14 -0300

    Finalização do CRUD

--- src/main/java/br/com/jagi/junitlearn/resource/UserResource.java
+++ src/main/java/br/com/jagi/junitlearn/resource/UserResource.java
@@ -1,18 +1,24 @@
 package br.com.jagi.junitlearn.resource;
 
-import br.com.jagi.junitlearn.domain.User;
 import br.com.jagi.junitlearn.service.UserService;
 import br.com.jagi.junitlearn.service.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserResource {
@@ -20,7 +26,28 @@ public class UserResource {
     private final UserService userService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> findById(@PathVariable("userId") Integer userId) {
+    public ResponseEntity<UserDTO> findById(@PathVariable("userId") Integer userId) {
         return ResponseEntity.ok().body(userService.findById(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
+        return new ResponseEntity<>(userService.create(userDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDTO> update(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok().body(userService.update(userDTO));
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<Void> delete(@PathVariable("userId") Integer userId) {
+        userService.delete(userId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/jagi/junitlearn/service/UserService.java
+++ src/main/java/br/com/jagi/junitlearn/service/UserService.java
@@ -2,9 +2,18 @@ package br.com.jagi.junitlearn.service;
 
 import br.com.jagi.junitlearn.domain.User;
 import br.com.jagi.junitlearn.repositories.UserRepository;
+import br.com.jagi.junitlearn.service.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,8 +21,47 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper mapper;
 
-    public User findById(Integer userId) {
-        return userRepository.findById(userId).orElse(null);
+    public UserDTO findById(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario não encontrado."));
+
+        return mapper.map(user, UserDTO.class);
+    }
+
+    public List<UserDTO> findAll() {
+        List<User> users = userRepository.findAll();
+
+        return users.stream()
+                .map(user -> mapper.map(user, UserDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO create(UserDTO userDTO) {
+        validaEmailCadastrado(userDTO);
+        User user = userRepository.save(mapper.map(userDTO, User.class));
+        return mapper.map(user, UserDTO.class);
+    }
+
+    private void validaEmailCadastrado(UserDTO userDTO) {
+        Optional<User> user = userRepository.findByEmail(userDTO.getEmail());
+
+        if (user.isPresent() && !user.get().getId().equals(userDTO.getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "E-mail já cadastrado.");
+        }
+    }
+
+    public UserDTO update(UserDTO userDTO) {
+        if (Objects.isNull(userDTO.getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não encontrado para atualização");
+        }
+
+        return create(userDTO);
+    }
+
+    public void delete(Integer userId) {
+        findById(userId);
+        userRepository.deleteById(userId);
     }
 }
